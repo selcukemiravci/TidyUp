c661f1ad0f605b15823fe905d7bf17e07d659e96
Makeda Morris <64229732+mkaeda@users.noreply.github.com>
2023-10-21 20:29:14 -0600

    add clock in the background

--- src/Components/TidyUp.jsx
+++ src/Components/TidyUp.jsx
@@ -7,12 +7,14 @@ import Trash from './Trash/Trash'
 import Mat from './Mat/Mat'
 import Plant from './Plant/Plant'
 import Box from './Box/Box'
+import Clock from './Clock/Clock'
 
 class TidyUp extends React.Component {
   render() {
     return <>
         <div className="container">
             <img src={bgImg} alt="room with powder blue walls" />
+            <Clock/>
             <Trash />
             <Box />
             <Book />
